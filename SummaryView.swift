305e09db008860740c186c02f8dae0a4ad9128b8
Ben <benena2006@gmail.com>
2024-04-15 14:39:40 -0500

    Text View: " Ben ate a large hamburger."

---
+++ SummaryView.swift
@@ -0,0 +1,13 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Ben on 4/15/24.
+//
+
+import Foundation
+struct SummaryView: View {
+    var body: some View {
+        Text("Ben ate a large hamburger")
+    }
+}
