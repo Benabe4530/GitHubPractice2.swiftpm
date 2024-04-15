79e5fbe6ead2c61c16ecee77706f87c0d181c95a
Haasini Police <hpolice5296@stu.d214.org>
2024-04-15 14:23:33 -0500

    I created a new file names noun view and added a navigation link to verb view in the body

---
+++ NounView.swift
@@ -0,0 +1,16 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Haasini Police on 4/15/24.
+//
+
+import Foundation
+import SwiftUI
+struct NounView: View {
+    var body: some View {
+        NavigationLink("Verb view") {
+          //  VerbView()
+        }
+    }
+}
