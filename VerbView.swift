edc24e6bc807cda307c95bb7a71e71730d965ffe
Ben <benena2006@gmail.com>
2024-04-15 14:30:24 -0500

    New file named "VerbView" + NavigationLink named "AdjectiveView"

---
+++ VerbView.swift
@@ -0,0 +1,15 @@
+//
+//  VerbView.swift
+//  GitHubPractice2
+//
+//  Created by Ben on 4/15/24.
+//
+
+import Foundation
+struct VerbView: View {
+    var body: some View {
+        NavigationLink("AdjectiveView") {
+          //  AdjectiveView()
+        }
+    }
+}
